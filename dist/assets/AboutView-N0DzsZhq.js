472d28dce6dc077a8b0cd7d3802ab0b7748fd16f
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2023-11-29 21:03:11 +0700

    finger crossed

---
+++ dist/assets/AboutView-N0DzsZhq.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-lNY6y4Vi.js";const r={};function t(c,n){return null}const o=e(r,[["render",t]]);export{o as default};
--- dist/assets/AboutView-ZSdrb1GL.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-s0UOnQC_.js";const r={};function t(c,n){return null}const o=e(r,[["render",t]]);export{o as default};
