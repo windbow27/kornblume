a74145457e01928e8fb6f5de40a281e941c04062
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2023-12-15 23:23:54 +0700

    update

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -6,38 +6,17 @@
             <button @click="exportStores" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Export Data
             </button>
-    
+
             <input type="file" ref="fileInput" @change="importStores" accept=".json" class="ml-4" style="display: none;" />
             <button @click="triggerFileInput"
                 class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2">
                 Import Data
             </button>
         </div>
-        <h2 class="text-2xl text-white font-bold my-4 lg:my-6">Danger Zone</h2>
-        <p class="text-white">If you encounter any unexpected issues with the site, you can reset your data.</p>
-        <div class="flex justify-center items-center p-2">   
-            <button onclick="my_modal_2.showModal()"
-                class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2">
-                Reset Data
-            </button>
-        </div>
 
-        <dialog id="my_modal_2" class="modal">
-            <div class="modal-box custom-gradient-gray-blue">
-                <p class="pb-4">Once you delete your data, there is no going back.</p>
-                <p class="pb-4">Please be certain.</p>
-                <button
-                    @click="resetStores"
-                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2 float-right">
-                    Reset
-                </button>
-            </div>
-            <form method="dialog" class="modal-backdrop">
-                <button>close</button>
-            </form>
-        </dialog>
-<h2 class="text-2xl text-white font-bold my-4 lg:my-6">Danger Zone</h2>
-        <p class="text-white">If you encounter any unexpected issues with the site, you can reset your data. Sorry for the inconvenience.</p>
+        <h2 class="text-2xl text-white font-bold my-4 lg:my-6">Danger Zone</h2>
+        <p class="text-white">If you encounter any unexpected issues with the site, you can reset your data. Sorry for the
+            inconvenience.</p>
         <div class="flex justify-center items-center p-2">
             <button onclick="my_modal_2.showModal()"
                 class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2">
