33dcbea5466367bb0ab829854c406b2397e30ae1
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2023-12-15 23:18:27 +0700

    text fix. huge deploy

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -13,6 +13,28 @@
                 Import Data
             </button>
         </div>
+<h2 class="text-2xl text-white font-bold my-4 lg:my-6">Danger Zone</h2>
+        <p class="text-white">If you encounter any unexpected issues with the site, you can reset your data. Sorry for the inconvenience.</p>
+        <div class="flex justify-center items-center p-2">
+            <button onclick="my_modal_2.showModal()"
+                class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2">
+                Reset Data
+            </button>
+        </div>
+
+        <dialog id="my_modal_2" class="modal">
+            <div class="modal-box custom-gradient-gray-blue flex flex-col justify-center items-center">
+                <p class="pb-4 text-white text-center">Once you delete your data, there is no going back.</p>
+                <p class="pb-4 text-white text-center">Please be certain.</p>
+                <button @click="resetStores"
+                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2">
+                    Reset
+                </button>
+            </div>
+            <form method="dialog" class="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
     </div>
 </template>
   
