49836e90790c2645887cb3775177bb6ee8c906ed
Fran <wei840220@gmail.com>
2023-12-16 12:01:49 +0800

    feat: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +11,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    watch: {
+      usePolling: true, //set to ensure that hot reload is enabled
     }
   }
 })
