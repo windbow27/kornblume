56f0769f2cee718c7c91ed7ce120f2d316a6176a
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2023-12-18 22:00:58 +0700

    added sorting, indexing, last 6* pull, better icons

---
+++ src/components/tracker/TrackerArcanistIcon.vue
@@ -0,0 +1,47 @@
+<!-- eslint-disable no-unused-vars -->
+<script setup>
+// FIXME:
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps({
+    arcanist: {
+        type: Object,
+        required: true
+    },
+    pity: {
+        type: Number,
+        required: true
+    }
+});
+const getArcanistImagePath = (id) => {
+    return `images/arcanists/icon/${id}.png`;
+};
+</script>
+
+<template>
+    <div class="tooltip" :data-tip="arcanist.Name">
+        <div class="rounded-md overflow-hidden">
+            <div class="avatar relative flex items-center justify-center">
+                <div class="w-16 rounded" :class="{
+                    'bg-orange-300': arcanist.Rarity === 6,
+                    'bg-yellow-100': arcanist.Rarity === 5,
+                    'bg-purple-400': arcanist.Rarity === 4,
+                    'bg-sky-200': arcanist.Rarity === 3,
+                    'bg-green-200': arcanist.Rarity === 2
+                }">
+                    <img :src="getArcanistImagePath(arcanist.Id)" :alt="arcanist.Name" />
+                </div>
+                <div class="absolute text-white font-bold w-8 h-6 right-0 bottom-0 rounded-tl-lg text-sm opacity-90" :class="{
+                    'bg-success': pity < 40,
+                    'bg-warning': pity >= 40 && pity < 60,
+                    'bg-error': pity >= 60
+                }">
+                    <div class="flex items-center justify-center h-full">
+                        {{ pity }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- src/views/TrackerView.vue
+++ src/views/TrackerView.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import { ref, Ref } from 'vue'
+import { ref, computed, defineExpose, Ref } from 'vue'
 import { useDataStore } from '@/stores/dataStore';
 import Tesseract, { createWorker } from 'tesseract.js';
 import ArcanistIcon from '../components/arcanist/ArcanistIcon.vue';
+import TrackerArcanistIcon from '../components/tracker/TrackerArcanistIcon.vue';
 
 const fileInput = ref(null)
+const isImporting = ref(false);
 const text = ref('')
 const arcanists = useDataStore().arcanists;
-const pulls = ref<{ ArcanistName: string; Rarity: number; DateAndTime: string }[]>([]);
+const pulls = ref<{ ArcanistName: string; Rarity: number; DateAndTime: Date }[]>([]);
 
 const triggerFileInput = () => {
     // Trigger the file input programmatically
@@ -18,6 +20,7 @@ type clickHandler = (payload: Event) => void | undefined;
 const ocr: clickHandler = (payload: Event): void => {
     const fileList: FileList | null = (payload.target as HTMLInputElement).files;
     if (fileList) {
+        isImporting.value = true;
         (async (): Promise<void> => {
             const worker: Tesseract.Worker = await createWorker('eng');
             for (let i: number = 0; i < fileList.length; i++) {
@@ -25,15 +28,16 @@ const ocr: clickHandler = (payload: Event): void => {
                 if (file) {
                     const ret: Tesseract.RecognizeResult = await worker.recognize(file);
                     text.value = ret.data.text;
-                    console.log(text.value);
                 }
+                isImporting.value = false;
             }
             await worker.terminate();
 
-            // Assuming arcanists is an array of objects with a Name property
             const arcanistNames = arcanists.map(a => a.Name).join('|');
 
             const pattern: RegExp = new RegExp(`(?<ArcanistName>${arcanistNames})(\\((?<Rarity>\\d+)\\))?(?<BannerGroup>.*?)(?<Date>\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})`);
+
+            const currentPulls: { ArcanistName: string; Rarity: number; BannerGroup: string; DateAndTime: Date }[] = [];
 
             // Extract information from each line
             text.value.trim().split('\n').forEach((line) => {
@@ -44,71 +48,120 @@ const ocr: clickHandler = (payload: Event): void => {
                     const rarity: number = arcanist ? arcanist.Rarity : 0;
                     const bannerGroup: string = match.groups?.BannerGroup.trim() || '';
                     const dateTime: Date = new Date(match.groups?.Date || '');
-                    const formattedDate: string = dateTime.toLocaleDateString('en-GB', {
-                        day: '2-digit',
-                        month: '2-digit',
-                        year: 'numeric'
-                    });
-                    const formattedTime: string = dateTime.toLocaleTimeString('en-GB', {
-                        hour12: false,
-                        hour: '2-digit',
-                        minute: '2-digit'
-                    });
-                    const formattedDateTime: string = `${formattedDate} ${formattedTime}`;
 
                     // Create an object for each pull
                     const pull = {
                         ArcanistName: arcanistName,
                         Rarity: rarity,
                         BannerGroup: bannerGroup,
-                        DateAndTime: formattedDateTime
+                        DateAndTime: dateTime
                     };
 
                     // Add the pull to the array
-                    pulls.value.push(pull);
+                    currentPulls.push(pull);
                 }
             });
 
-            // Log the resulting array of pulls
-            console.log(pulls.value);
+            // console.log(currentPulls);
+
+            const isSubset = currentPulls.every(currentPull =>
+                pulls.value.some(pull => isEqualPull(pull, currentPull))
+            );
+            // console.log(isSubset);
+
+            if (!isSubset) {
+                // If currentPulls is not a subset of pulls, add all currentPulls to pulls
+                pulls.value.push(...currentPulls);
+            }
         })();
     }
 }
+
+const isEqualPull = (pull1, pull2) => {
+    return pull1.ArcanistName === pull2.ArcanistName &&
+        pull1.DateAndTime.getTime() === pull2.DateAndTime.getTime() &&
+        pull1.Rarity === pull2.Rarity;
+}
+
+const formatDate = (date: Date): string => {
+    const formattedDate: string = date.toLocaleDateString('en-GB', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+    const formattedTime: string = date.toLocaleTimeString('en-GB', {
+        hour12: false,
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+    return `${formattedDate} ${formattedTime}`;
+}
+
+const indexedPulls = computed(() => {
+    const sortedPulls = pulls.value.slice().sort((a, b) => new Date(b.DateAndTime).getTime() - new Date(a.DateAndTime).getTime());
+    return sortedPulls.map((pull, index) => {
+        return {
+            PullNumber: sortedPulls.length - index,
+            ArcanistName: pull.ArcanistName,
+            Rarity: pull.Rarity,
+            DateAndTime: pull.DateAndTime
+        }
+    });
+});
+
+const sixStarsPullsList = computed(() => {
+    const sixStarPulls = indexedPulls.value
+        .filter(pull => pull.Rarity === 6)
+        .sort((a, b) => a.PullNumber - b.PullNumber)
+        .map(pull => pull.PullNumber);
+    return sixStarPulls.map((pullNumber, index) => index === 0 ? pullNumber : pullNumber - sixStarPulls[index - 1]);
+});
+
+defineExpose({
+    formatDate
+})
+
 </script>
 
 <template>
     <div class="responsive-spacer">
         <h2 class="text-2xl text-white font-bold mb-4 lg:mb-6">Summon Tracker</h2>
-        <input type="file" ref="fileInput" @change="ocr" accept=".jpg" class="ml-4" style="display: none;" multiple />
-        <button @click="triggerFileInput"
-            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2">
-            OCR Import
-        </button>
+        <div class="space-x-3">
+            <input type="file" ref="fileInput" @change="ocr" accept="image/*" class="ml-4" style="display: none;"
+                multiple />
+            <button @click="triggerFileInput"
+                class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2">
+                OCR Import
+            </button>
+            <i v-show="isImporting" class="text-white text-2xl text-center fa-solid fa-spinner fa-spin-pulse"></i>
+        </div>
         <p class=" text-white font-bold text-xl text-center">Summon Summary</p>
-        <div class="flex flex-col text-white p-4 gap-2 max-w-xs mx-auto">
+        <div class="flex flex-col text-white p-4 gap-2 max-w-sm mx-auto">
             <div class="flex justify-between">
                 <div class="text">Total Summons:</div>
-                <div class="number">{{ pulls.length }}</div>
+                <div class="number">{{ indexedPulls.length }}</div>
             </div>
             <div class="flex justify-between">
                 <div class="text">6 <i class="fa-solid fa-star text-orange-300"></i> Summons:</div>
-                <div class="number">{{ pulls.filter(p => p.Rarity === 6).length }}</div>
+                <div class="number">{{ indexedPulls.filter(p => p.Rarity === 6).length }}</div>
             </div>
             <div class="flex justify-between">
                 <div class="text">5 <i class="fa-solid fa-star text-yellow-100"></i> Summons:</div>
-                <div class="number">{{ pulls.filter(p => p.Rarity === 5).length }}</div>
+                <div class="number">{{ indexedPulls.filter(p => p.Rarity === 5).length }}</div>
             </div>
-            <div v-if="pulls.filter(p => p.Rarity === 6).length > 0" class="flex justify-between">
+            <div v-if="indexedPulls.filter(p => p.Rarity === 6).length > 0" class="flex justify-between">
                 <div class="text">AVG 6 <i class="fa-solid fa-star text-orange-300"></i> Pity:</div>
-                <div class="number">{{ Math.floor(pulls.length / pulls.filter(p => p.Rarity === 6).length) }}</div>
+                <div class="number">{{ Math.floor(indexedPulls.length / indexedPulls.filter(p => p.Rarity === 6).length) }}
+                </div>
             </div>
             <div v-else class="flex justify-between">
                 <div class="text">AVG 6 <i class="fa-solid fa-star text-orange-300"></i> Pity:</div>
                 <div class="number">0</div>
             </div>
-            <div v-if="pulls.filter(p => p.Rarity === 5).length > 0" class="flex justify-between">
+            <div v-if="indexedPulls.filter(p => p.Rarity === 5).length > 0" class="flex justify-between">
                 <div class="text">AVG 5 <i class="fa-solid fa-star text-yellow-100"></i> Pity:</div>
-                <div class="number">{{ Math.floor(pulls.length / pulls.filter(p => p.Rarity === 5).length) }}</div>
+                <div class="number">{{ Math.floor(indexedPulls.length / indexedPulls.filter(p => p.Rarity === 5).length) }}
+                </div>
             </div>
             <div v-else class="flex justify-between">
                 <div class="text">AVG 5 <i class="fa-solid fa-star text-yellow-100"></i> Pity:</div>
@@ -119,21 +172,23 @@ const ocr: clickHandler = (payload: Event): void => {
         <div
             class="w-full items-center custom-gradient-gray-blue rounded border border-blue-900 justify-center px-4 pt-4 pb-2">
             <div class="text text-center pb-4">Recent 6 <i class="fa-solid fa-star text-orange-300"></i> Summons</div>
-            <div class="flex justify-center space-x-3">
+            <div class="flex flex-wrap justify-center space-x-3">
                 <!-- Fix the key later -->
-                <div v-for="pull in pulls.filter(p => p.Rarity === 6).slice(0, 5)"
+                <div v-for="(pull, index) in indexedPulls.filter(p => p.Rarity === 6)"
                     :key="`${pull.DateAndTime}-${pull.ArcanistName}`">
-                    <ArcanistIcon :arcanist="arcanists.find(a => a.Name === pull.ArcanistName)" />
+                    <TrackerArcanistIcon :arcanist="arcanists.find(a => a.Name === pull.ArcanistName)"
+                        :pity="sixStarsPullsList[sixStarsPullsList.length - 1 - index]" />
                 </div>
             </div>
         </div>
 
         <!-- Summon History -->
-        <div>
-            <div class="text text-center text-xl pb-4 pt-10">Summon History</div>
-            <div v-for="(pull, index) in pulls" :key="`${pull.DateAndTime}-${pull.ArcanistName}-${index}`"
-                class="grid grid-cols-3 justify-between items-center p-2 border-b border-gray-200 max-w-lg m-auto">
-                <div class="flex items-center">
+        <div class="text text-center text-xl pb-4 pt-10">Summon History</div>
+        <div class="max-w-lg m-auto">
+            <div v-for="(pull, index) in indexedPulls" :key="`${pull.DateAndTime}-${pull.ArcanistName}-${index}`"
+                class="grid grid-cols-3 justify-between items-center p-2 border-b border-gray-200 space-x-5">
+                <div class="flex items-center space-x-5">
+                    <div class="text-white">{{ pull.PullNumber }} </div>
                     <ArcanistIcon :arcanist="arcanists.find(a => a.Name === pull.ArcanistName)" />
                     <div class="ml-2" :class="{
                         'text-orange-300': pull.Rarity === 6,
@@ -143,14 +198,14 @@ const ocr: clickHandler = (payload: Event): void => {
                         'text-green-200': pull.Rarity === 2
                     }">{{ pull.ArcanistName }}</div>
                 </div>
-                <div :class="{
+                <div class="text-center" :class="{
                     'text-orange-300': pull.Rarity === 6,
                     'text-yellow-100': pull.Rarity === 5,
                     'text-purple-400': pull.Rarity === 4,
                     'text-sky-200': pull.Rarity === 3,
                     'text-green-200': pull.Rarity === 2
                 }">{{ pull.Rarity }} <i class="fa-solid fa-star"></i></div>
-                <div class="text-white">{{ pull.DateAndTime }}</div>
+                <div class="text-white"> {{ formatDate(pull.DateAndTime) }}</div>
             </div>
         </div>
 
