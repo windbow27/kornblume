e3661b1bf0810a328f48f895df8b06fd73f98fa6
Fran <wei840220@gmail.com>
2023-12-23 21:20:59 +0800

    refactor

--- src/components/planner/result/PlannerCard.vue
+++ src/components/planner/result/PlannerCard.vue
@@ -33,18 +33,26 @@ watch(shouldHideScrollbar, (newVal) => {
         </div>
 
         <div class="tooltip w-full" data-tip="Number of runs | Activities | Days">
-            <div
-                class="calculations flex justify-center items-center bg-gradient-to-r from-blue-800/70 to-blue-900/90 text-white p-2 rounded-md">
+            <div class="calculations flex justify-center items-center bg-gradient-to-r from-blue-800/70 to-blue-900/90 text-white p-2 rounded-md">
                 <p class="border-blue-700/90 border-r pr-3">
-                    <span v-if="card.runs > 0">
-                        <i class="fa-solid fa-xmark text-xs mr-0.5"></i>{{ card.runs }}
+                    <span v-show="card.runs > 0">
+                        <i class="fa-solid fa-xmark text-xs mr-0.5" />{{ card.runs }}
                     </span>
-                    <span v-else>{{ card.runs }}</span>
                 </p>
-                <p class="pl-2">{{ card.activity == null ? 'Crafted from available Materials. List includes dependencies. ' : card.activity }}</p>
+
+                <p class="pl-2">
+                    {{ card.activity === 0 ?
+                        'Crafted from available Materials. List includes dependencies.':
+                        card.activity
+                    }}
+                </p>
                 <img v-show="card.activity" :src="card.activityImagePath" alt="Activity Image" class="inline-block w-8 h-8" />
-                <p class="border-blue-700/90 border-l pl-3">{{ card.days }} {{ card.days === null ? '' : card.days > 1 ? 'days'
-                    : 'day' }}</p>
+
+                <p class="border-blue-700/90 border-l pl-3">
+                    <span v-show="card.days > 0">
+                        {{ card.days }} {{ card.days === 0 ? '' : card.days > 1 ? 'days': 'day' }}
+                    </span>
+                </p>
             </div>
         </div>
 
