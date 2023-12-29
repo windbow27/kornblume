693901427e4ee63801e78ffc219c4ddfe5ec26a0
Fran <wei840220@gmail.com>
2023-12-29 16:45:37 +0800

    feat: translate profile view

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,34 +1,29 @@
 <template>
     <div class="responsive-spacer">
         <div class="pb-6">
-            <h2 class="text-2xl text-white font-bold mb-2 lg:mb-4">Profile</h2>
-            <p class="text-white"> You can export or import your data here.</p>
+            <h2 class="text-2xl text-white font-bold mb-2 lg:mb-4">{{ $t('profile') }}</h2>
+            <p class="text-white"> {{ $t('you-can-export-or-import-your-data-here') }}</p>
             <div class="flex justify-center items-center p-2 space-x-5">
                 <button @click="exportStores" class="btn btn-info text-black font-bold py-2 px-4 rounded">
-                    Export Data
-                </button>
+                    {{ $t('export-data') }} </button>
 
                 <input type="file" ref="fileInput" @change="importStores" accept=".json" class="ml-4" style="display: none;" />
                 <button @click="triggerFileInput"
                     class="btn btn-success text-black font-bold py-2 px-4 rounded ml-2">
-                    Import Data
-                </button>
+                    {{ $t('import-data') }} </button>
             </div>
         </div>
 
         <div class="pb-6">
-            <h2 class="text-2xl text-white font-bold mb-2 lg:mb-4">Danger Zone</h2>
-            <p class="text-white">If you encounter any unexpected issues with the site, you can reset your data. Sorry for the
-                inconvenience.</p>
+            <h2 class="text-2xl text-white font-bold mb-2 lg:mb-4">{{ $t('danger-zone') }}</h2>
+            <p class="text-white">{{ $t('if-you-encounter-any-unexpected-issues-with-the-site-you-can-reset-your-data-sorry-for-the-inconvenience') }}</p>
             <div class="flex justify-center items-center p-2 space-x-5">
                 <button onclick="resetTracker.showModal()"
                     class="btn btn-error text-black font-bold py-2 px-4 rounded ml-2">
-                    Reset Tracker
-                </button>
+                    {{ $t('reset-tracker') }} </button>
                 <button onclick="resetAll.showModal()"
                     class="btn btn-error text-black font-bold py-2 px-4 rounded ml-2">
-                    Reset Data
-                </button>
+                    {{ $t('reset-data') }} </button>
             </div>
 
             <dialog id="resetTracker" class="modal">
@@ -36,12 +31,11 @@
                     <form method="dialog">
                         <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-white">✕</button>
                     </form>
-                    <p class="pb-4 text-white text-center">Once you delete your Summon Tracker data, there is no going back.</p>
-                    <p class="pb-4 text-white text-center">Please be certain.</p>
+                    <p class="pb-4 text-white text-center">{{ $t('once-you-delete-your-summon-tracker-data-there-is-no-going-back') }}</p>
+                    <p class="pb-4 text-white text-center">{{ $t('please-be-certain') }}</p>
                     <button @click="resetTracker"
                         class="btn btn-error text-black font-bold py-2 px-4 rounded ml-2">
-                        Reset Tracker
-                    </button>
+                        {{ $t('reset-tracker') }} </button>
                 </div>
                 <form method="dialog" class="modal-backdrop">
                     <button>close</button>
@@ -53,15 +47,14 @@
                     <form method="dialog">
                         <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-white">✕</button>
                     </form>
-                    <p class="pb-4 text-white text-center">Once you delete your data, there is no going back.</p>
-                    <p class="pb-4 text-white text-center">Please be certain.</p>
+                    <p class="pb-4 text-white text-center">{{ $t('once-you-delete-your-data-there-is-no-going-back') }}</p>
+                    <p class="pb-4 text-white text-center">{{ $t('please-be-certain') }}</p>
                     <button @click="resetStores"
                         class="btn btn-error text-black font-bold py-2 px-4 rounded ml-2">
-                        Reset All
-                    </button>
+                        {{ $t('reset-all') }} </button>
                 </div>
                 <form method="dialog" class="modal-backdrop">
-                    <button>close</button>
+                    <button>{{ $t('close') }}</button>
                 </form>
             </dialog>
         </div>
