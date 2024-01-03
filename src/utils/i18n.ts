0c291cf6146f6d25a744288c46150e627a23d231
Fran <wei840220@gmail.com>
2024-01-03 20:00:55 +0800

    feat: add i18n pre-compiler

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,25 +1,54 @@
 import { createI18n } from 'vue-i18n'
+import messages from '@intlify/unplugin-vue-i18n/messages';
 
-import enUS from '../../lang/en-US.json'
-import arcanistsEnUS from '../../lang/static/arcanists/en-US.json'
-import itemsEnUS from '../../lang/static/items/en-US.json'
-import stagesEnUs from '../../lang/static/stages/en-US.json'
+// List of all supported lang codes, please update this list when new language is added
+export const supportedLangCodes = ['en-US', 'de-DE', 'zh-CN', 'zh-TW', 'vi-VN', 'id-ID', 'ko-KR', 'ja-JP'];
 
-import zhTW from '../../lang/zh-TW.json'
-import arcanistsZhTW from '../../lang/static/arcanists/zh-TW.json'
-import itemsZhTW from '../../lang/static/items/zh-TW.json'
-import stagesZhTW from '../../lang/static/stages/zh-TW.json'
+export const langDropdownOptions = [
+    {
+        locale: 'en-US',
+        text: 'English',
+        icon: 'fi fi-us'
+    },
+    {
+        locale: 'de-DE',
+        text: 'Deutsch',
+        icon: 'fi fi-de'
+    },
+    {
+        locale: 'zh-CN',
+        text: '简体中文',
+        icon: 'fi fi-cn'
+    },
+    {
+        locale: 'zh-TW',
+        text: '繁體中文',
+        icon: 'fi fi-tw'
+    },
+    {
+        locale: 'vi-VN',
+        text: 'Tiếng Việt',
+        icon: 'fi fi-vn'
+    },
+    {
+        locale: 'id-ID',
+        text: 'Indonesia',
+        icon: 'fi fi-id'
+    },
+    {
+        locale: 'ko-KR',
+        text: '한국어',
+        icon: 'fi fi-kr'
+    },
+    {
+        locale: 'ja-JP',
+        text: '日本語',
+        icon: 'fi fi-jp'
+    }
+]
 
-import zhCN from '../../lang/zh-CN.json'
-import arcanistsZhCN from '../../lang/static/arcanists/zh-CN.json'
-import itemsZhCN from '../../lang/static/items/zh-CN.json'
-import stagesZhCN from '../../lang/static/stages/zh-CN.json'
-
-import jaJP from '../../lang/ja-JP.json'
-import koKR from '../../lang/ko-KR.json'
-import viVN from '../../lang/vi-VN.json'
-import deDE from '../../lang/de-DE.json'
-import idID from '../../lang/id-ID.json'
+// List of all supported lang codes for static files (for arcanists/items/stages data)
+const supportedStaticLangCodes = ['en-US', 'zh-TW', 'zh-CN'];
 
 const getI18nKeyFromText = (str: string) => {
     return str.replaceAll('.', '').replaceAll(' ', '-').toLowerCase()
@@ -36,41 +65,47 @@ const i18n = createI18n({
     fallbackLocale: 'en-US',
     globalInjection: true,
     messageResolver,
-    messages: {
-        'en-US': {
-            ...enUS,
-            ...arcanistsEnUS,
-            ...itemsEnUS,
-            ...stagesEnUs
-        },
-        'zh-TW': {
-            ...zhTW,
-            ...arcanistsZhTW,
-            ...itemsZhTW,
-            ...stagesZhTW
-        },
-        'zh-CN': {
-            ...zhCN,
-            ...arcanistsZhCN,
-            ...itemsZhCN,
-            ...stagesZhCN
-        },
-        'ja-JP': {
-            ...jaJP
-        },
-        'ko-KR': {
-            ...koKR
-        },
-        'vi-VN': {
-            ...viVN
-        },
-        'de-DE': {
-            ...deDE
-        },
-        'id-ID': {
-            ...idID
-        }
-    }
+    messages
 })
+
+// Set new locale
+export async function setLocale (locale) {
+    // Load locale if not available yet
+    const lang = supportedLangCodes.includes(locale) ? locale : 'en-US'
+    const staticLang = supportedStaticLangCodes.includes(locale) ? locale : 'en-US'
+
+    if (!i18n.global.availableLocales.includes(lang)) {
+        const messages = await loadLocale(`../../lang/${lang}.json`);
+        const arcanists = await loadLocale(`../../lang/static/arcanists/${staticLang}.json`);
+        const items = await loadLocale(`../../lang/static/items/${staticLang}.json`);
+        const stages = await loadLocale(`../../lang/static/stages/${staticLang}.json`);
+
+        // Add locale
+        i18n.global.setLocaleMessage(locale, {
+            ...messages,
+            ...arcanists,
+            ...items,
+            ...stages
+        });
+    }
+
+    // Set locale
+    i18n.global.locale.value = locale;
+    localStorage.setItem('locale', locale);
+}
+
+// Fetch locale
+function loadLocale (path) {
+    return fetch(path)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(`Failed to fetch locale json file: ${path}`);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
 
 export default i18n
--- vite.config.mts
+++ vite.config.mts
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import { fileURLToPath, URL } from 'node:url'
+import { resolve, dirname } from 'node:path'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/Kornblume/',
     plugins: [
         vue(),
+        VueI18nPlugin({
+            // locale messages resource pre-compile option
+            include: resolve(dirname(fileURLToPath(import.meta.url)), './lang/**'),
+            runtimeOnly: false,
+            strictMessage: false
+        }),
         eslintPlugin({ cache: false })
     ],
     resolve: {
