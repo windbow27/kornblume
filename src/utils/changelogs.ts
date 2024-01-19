a3f73385e1536ae5ed3e13af8bbcbfcb8314d725
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2024-01-19 13:10:39 +0700

    mhmh iOS

--- src/utils/changelogs.ts
+++ src/utils/changelogs.ts
@@ -1,5 +1,6 @@
 export const changelogs = [
     // Add more from top
+    { date: '18/01/2024', description: '• Fixed iOS unable to import images.' },
     { date: '17/01/2024', description: '• Added Kaalaa Baunaa and Shamane banners.\n • Updated 1.3 part 1 Event Shop.' },
     { date: '16/01/2024', description: '• Added Centurion banner.' },
     { date: '15/01/2024', description: '• Added "Level Up" function where you can consume your warehouse materials to update arcanist to goal status. Try it out by left clicking an arcanist.' },
