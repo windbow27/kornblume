64ce462928e4b07558a4f19648ae742cef04e744
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2024-01-28 08:19:57 +0700

    filter + ui improvements

--- src/components/arcanist/ArcanistPortrait.vue
+++ src/components/arcanist/ArcanistPortrait.vue
@@ -28,12 +28,17 @@ const getArcanistAfflatusPath = (afflatus: string) => {
 <template>
     <div class="p-2.5 relative overflow-hidden group">
         <div class="transform transition-transform duration-500 overflow-hidden">
-            <img class="w-16 sm:w-20 rounded-b-full transform transition-transform duration-500 group-hover:scale-110"
+            <img class="w-16 sm:w-20 rounded-b-full scale-105 transform transition-transform duration-500 group-hover:scale-125"
                 :src="getArcanistImagePath(props.arcanist.Id)" :alt="props.arcanist.Name" />
             <span v-if="count >= 0"
                 class="absolute top-2 left-1 w-6 text-center text-sm font-bold text-white/90 bg-opacity-50 rounded-md bg-black">
-                E{{ props.count }} </span>
-            <img class="absolute top-2 right-1 w-4 rounded-md opacity-90 bg-blue-900"
+                <i18n-t keypath='P{portrait}'>
+                    <template #portrait>
+                        <span> {{ props.count }}</span>
+                    </template>
+                </i18n-t>
+            </span>
+            <img class="absolute top-2 right-1 w-4 rounded-md opacity-90"
                 :src="getArcanistAfflatusPath(props.arcanist.Afflatus)" alt="">
             <img class="absolute bottom-0 w-16 sm:w-20 rounded-md" :src="getArcanistFramePath(props.arcanist.Rarity)"
                 alt="">
--- src/views/ArcanistsView.vue
+++ src/views/ArcanistsView.vue
@@ -11,6 +11,24 @@ const arcanistStore = useDataStore().arcanists;
 const listArcanists = ref<IArcanist[]>([]);
 const searchQuery = ref('');
 const pulls = ref<IPull[]>([]);
+const activeRarities = ref<number[]>([]);
+const activeAfflatus = ref<string[]>([]);
+
+const selectedRarities = (rarity: number) => {
+    if (activeRarities.value.includes(rarity)) {
+        activeRarities.value = activeRarities.value.filter(r => r !== rarity);
+    } else {
+        activeRarities.value.push(rarity);
+    }
+}
+
+const selectedAfflatus = (afflatus: string) => {
+    if (activeAfflatus.value.includes(afflatus)) {
+        activeAfflatus.value = activeAfflatus.value.filter(a => a !== afflatus);
+    } else {
+        activeAfflatus.value.push(afflatus);
+    }
+}
 
 const portraitCounts = computed(() => {
     return listArcanists.value.map(arc => ({
@@ -20,12 +38,24 @@ const portraitCounts = computed(() => {
 });
 
 const filteredArcanists = computed(() => {
-    // Filter arcanists based on the searchQuery
+    // Filter arcanists based on the searchQuery and the selected rarities
+    let filtered = listArcanists.value;
+
     if (locale.value === 'en-US') {
-        return listArcanists.value.filter(arc => arc.Name.toLowerCase().includes(searchQuery.value.toLowerCase()));
+        filtered = filtered.filter(arc => arc.Name.toLowerCase().includes(searchQuery.value.toLowerCase()));
     } else {
-        return listArcanists.value.filter(arc => t(arc.Name).includes(searchQuery.value));
+        filtered = filtered.filter(arc => t(arc.Name).includes(searchQuery.value));
     }
+
+    if (activeRarities.value.length > 0) {
+        filtered = filtered.filter(arc => activeRarities.value.includes(arc.Rarity));
+    }
+
+    if (activeAfflatus.value.length > 0) {
+        filtered = filtered.filter(arc => activeAfflatus.value.includes(arc.Afflatus));
+    }
+
+    return filtered;
 });
 
 onMounted(() => {
@@ -58,16 +88,31 @@ watchEffect(() => {
         <div class="flex pb-4">
             <h2 class="text-2xl text-white font-bold">{{ $t('arcanists') }}</h2>
         </div>
-        <div class="flex p-4 gap-x-4">
+        <div class="flex flex-wrap p-4 gap-x-4 justify-center">
             <input v-model="searchQuery" type="text" :placeholder="$t('search-arcanists')"
-                class="bg-gray-800 text-white p-2 rounded-md focus:outline-none">
-            <!-- <div class="form-control">
-                <label class="cursor-pointer label justify-center space-x-5">
-                    <span class="label-text text-white text-md">{{ $t('show-unreleased-arcanists') }}</span>
-                    <input v-model="usePlannerSettingsStore().settings.showUnreleasedArcanists" type="checkbox"
-                        class="checkbox checkbox-info" />
-                </label>
-            </div> -->
+        class="w-full lg:w-auto bg-gray-800 text-white p-2 rounded-md focus:outline-none">
+            <!-- Rarity select -->
+            <div class="flex justify-center space-x-2">
+                <button v-for="i in [2, 3, 4, 5, 6]" :key="i"
+                    :class="{ 'border-2 border-info': activeRarities.includes(i), 'border-2 border-transparent': !activeRarities.includes(i) }"
+                    @click="selectedRarities(i)" class="p-2 rounded-md">
+                    <i class="fa-solid fa-star" :class="{
+                        'text-orange-300': i === 6,
+                        'text-yellow-100': i === 5,
+                        'text-purple-400': i === 4,
+                        'text-sky-200': i === 3,
+                        'text-green-200': i === 2
+                    }"></i>
+                </button>
+            </div>
+            <!-- Afflatus select -->
+            <div class="flex justify-center space-x-2">
+                <button v-for="afflatus in ['Beast', 'Mineral', 'Plant', 'Star', 'Intellect', 'Spirit']" :key="afflatus"
+                    :class="{ 'border-2 border-info': activeAfflatus.includes(afflatus), 'border-2 border-transparent': !activeAfflatus.includes(afflatus) }"
+                    @click="selectedAfflatus(afflatus)" class="p-2 rounded-md">
+                    <img class="w-4" :src="`images/arcanists/misc/${afflatus.toLowerCase()}.png`" alt="">
+                </button>
+            </div>
         </div>
         <div class="flex flex-wrap items-center justify-center">
             <ArcanistPortrait v-for="arcanist in filteredArcanists" :key="arcanist.Id" :arcanist="arcanist"
