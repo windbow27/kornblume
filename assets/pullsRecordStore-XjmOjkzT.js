1ad864a4d8849b6cd4c58faae5ef3346b662a4b9
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2024-03-06 08:05:22 +0700

    Updates

---
+++ assets/pullsRecordStore-XjmOjkzT.js
@@ -0,0 +1,1 @@
+import{J as e}from"./index-SjWV0c8e.js";const a=e("pulls",{state:()=>({data:[]}),actions:{updatePullsRecord(t){this.data=[...t]},reset(){this.data=[]}},persist:!0});export{a as u};
--- assets/pullsRecordStore-nvSwc2al.js
+++
@@ -1,1 +0,0 @@
-import{J as e}from"./index-NvrH3v6t.js";const a=e("pulls",{state:()=>({data:[]}),actions:{updatePullsRecord(t){this.data=[...t]},reset(){this.data=[]}},persist:!0});export{a as u};
