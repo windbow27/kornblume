374922195d50e382e5337905008ff3c0d731933d
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2024-03-10 18:14:59 +0700

    Updates

--- assets/HomeView-cH0Tb_dY.js
+++ assets/HomeView-cH0Tb_dY.js
@@ -1,4 +1,4 @@
-import{o as $}from"./index-5xhbhsAD.js";import{d as x,o,c as n,a as e,t,F as g,r as v,u as F,b as W,e as R,f as _,g as u,w as y,h as j,i as m,j as M,k as O,l as w,m as z}from"./index-XYSabtjj.js";const k=[{date:"10/03/2024",description:`• UI "fixes". 
+import{o as $}from"./index-iR1m_zAr.js";import{d as x,o,c as n,a as e,t,F as g,r as v,u as F,b as W,e as R,f as _,g as u,w as y,h as j,i as m,j as M,k as O,l as w,m as z}from"./index-FIXZlUyh.js";const k=[{date:"10/03/2024",description:`• UI "fixes". 
  • Let me know if anything looks funky.`},{date:"09/03/2024",description:`• Added Tracker Editor. 
  • Updated stats for released arcanists by Heightweight.`},{date:"08/03/2024",description:`• Added Google Drive Sync. 
  • Added The First Milestone banner.`},{date:"05/03/2024",description:"• Planner has been modified and upgraded. Enjoy almost lag free planning experience."},{date:"04/03/2024",description:`• Cleaning up item names in json and bug fixes. 
