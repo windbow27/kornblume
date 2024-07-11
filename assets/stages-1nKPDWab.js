e3c1e3fc57a6a00370f65e6de12a2f259775186f
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2024-07-11 12:27:24 +0700

    Updates

--- assets/stages-1nKPDWab.js
+++
@@ -1,1 +0,0 @@
-import{h as e}from"./images-Wij9r9Cd.js";function o(t,a){return{stage:a,stageImagePath:e(t.id)}}function s(t){return t.drops?Object.entries(t.drops).map(([a,r])=>({Material:a,Quantity:r/t.count})):[]}export{s as a,o as n};
