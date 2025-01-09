e42c097ccec7fd3d9de57a04aa0ce3943d05f284
Fran <wei840220@gmail.com>
2025-01-09 09:54:25 +0800

    Updates

---
+++ assets/stages-DJE707qp.js
@@ -0,0 +1,1 @@
+import{j as e}from"./images-DMuE_yEt.js";function o(t,a){return{stage:a,stageImagePath:e(t.id)}}function s(t){return t.drops?Object.entries(t.drops).map(([a,r])=>({Material:a,Quantity:r/t.count})):[]}export{s as a,o as n};
