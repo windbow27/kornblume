a49e86a419aa020b959e809ec0f0c706a284a42a
windbow27 <phonglunhi27@gmail.com>
2025-02-10 14:20:20 +0700

    Updates

--- assets/stages-Bq_cBXzZ.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./images-B-aLLcK9.js";function o(t,a){return{stage:a,stageImagePath:e(t.id)}}function s(t){return t.drops?Object.entries(t.drops).map(([a,r])=>({Material:a,Quantity:r/t.count})):[]}export{s as a,o as n};
