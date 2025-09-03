1606c99570d8c1ab05ad4fc7e78a9acf2949a294
Kalium <kalium@kalium.dev>
2025-09-02 22:33:32 -0400

    Turn quantity function into a reactive computed value

--- src/components/item/ItemGoalIcon.vue
+++ src/components/item/ItemGoalIcon.vue
@@ -11,12 +11,9 @@ const props = defineProps({
     }
 });
 
-const checkQuantity = () => {
-    if (props.material.Quantity > 99 || useWarehouseStore().getItemQuantity(props.material.Material) > 99) {
-        return true;
-    }
-    return false;
-};
+const isWide = computed(() => {
+    return props.material.Quantity > 99 || useWarehouseStore().getItemQuantity(props.material.Material) > 99;
+});
 
 const getNormalizedMaterial = computed(() => {
     return normalizeDisplayMaterial(props.material);
@@ -28,12 +25,12 @@ const getNormalizedMaterial = computed(() => {
     <div class="tooltip" :data-tip="$t(getNormalizedMaterial.material)">
         <div class="relative inline-block">
             <img :src="getNormalizedMaterial.borderImagePath" alt="Border Image" class="w-20 h-20 absolute"
-                :class="{ 'w-36': checkQuantity() }" />
+                :class="{ 'w-36': isWide }" />
             <img :src="getNormalizedMaterial.itemImagePath" alt="Material Image" class="w-20 h-20 avatar"
-                :class="{ 'mx-5': checkQuantity() }" />
+                :class="{ 'mx-5': isWide }" />
             <div class="flex items-center justify-center small-text text-white absolute -bottom-3 left-3 input input-xs rounded-t-none"
                 :class="{
-        'left-[17px] w-[85px]': checkQuantity(), 'w-14': !checkQuantity(),
+        'left-[17px] w-[85px]': isWide, 'w-14': !isWide,
         'bg-green-800/80': useWarehouseStore().getItemQuantity(props.material.Material) >= props.material.Quantity, 'bg-red-500/60': useWarehouseStore().getItemQuantity(props.material.Material) < props.material.Quantity
     }">
                 {{ formatQuantity(useWarehouseStore().getItemQuantity(getNormalizedMaterial.material)) }} / {{
