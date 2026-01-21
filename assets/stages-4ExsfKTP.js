67ef92e1140feb9ab06da3e1badf83e1ba9dd445
windbow <windbow@email.com>
2026-01-21 21:20:29 +0700

    Updates

--- assets/stages-4ExsfKTP.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./images-xvwOw9Fc.js";function o(t,a){return{stage:a,stageImagePath:e(t)}}function s(t){return t.drops?Object.entries(t.drops).map(([a,r])=>({Material:a,Quantity:r/t.count})):[]}export{s as a,o as n};
